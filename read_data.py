c56a094c962b74a0f0489df2961d49b3048dd1a9
Davron17 <dorazaliyev75@gmail.com>
2023-11-02 11:40:22 +0500

    read

--- read_data.py
+++ read_data.py
@@ -1,13 +1,19 @@
 import json
-def read_data(file_path: str)->dict:
+
+def read_data(file_path: str) -> dict:
     """
     This function will read the json file and return the data as a dictionary.
-    
+
     Parameters:
         file_path (str): Path of the file to be read
     Returns:
         dict: Dictionary containing the data of the json file.
-    
+
     """
-    #open file
-    return
+    f=open(file_path, 'r', encoding='UTF8')
+    d = json.load(f)
+    return d
+
+data = "result.json"
+total = read_data(data)
+print(total)
