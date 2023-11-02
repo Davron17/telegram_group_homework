ca633e82fd98d8a8a4115c2ee4e5d65a990ce737
Davron17 <dorazaliyev75@gmail.com>
2023-11-02 12:00:37 +0500

    message

--- find_user_message_count.py
+++ find_user_message_count.py
@@ -1,14 +1,27 @@
-from read_data import read_data
+from read_data import read_data 
 from find_all_users_id import find_all_users_id
 
-def find_user_message_count(data: dict, users_id: str)->dict:
+def find_user_message_count(data: dict, user_ids: list) -> dict:
     """
     This function will find the user's message count.
-    
+
     Parameters:
-        data (dict): Dictionary containing the data of the json file
-        user_id (list): User id of the user
+        data (dict): Dictionary containing the data of the JSON file
+        user_ids (list): User IDs of the users (as a list)
     Returns:
-        dict: Number of messages of the users
+        dict: Number of messages for each user
     """
-    return
+
+    message_count = {}
+
+    for message in data['messages']:
+        if message['user_id'] in user_ids:
+            user_id = message['user_id']
+            if user_id in message:
+                message[user_id] += 1
+            else:
+                message[user_id] = 1
+
+    return message
+
+data = get("result.json")
