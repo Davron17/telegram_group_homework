e3decca92c29c6eeea553f9761652fdcf13d3cda
Davron17 <dorazaliyev75@gmail.com>
2023-11-04 14:20:08 +0500

    id

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -1,4 +1,4 @@
-from read_data import read_data
+from read_data import read_data as read
 
 def find_all_users_id(data: dict)->list:
     """ 
@@ -9,4 +9,14 @@ def find_all_users_id(data: dict)->list:
     Returns:
         list: List containing all the users id
     """
-    return
+    l=[]
+    for i in data["messages"]:
+        if "actor_id" in i and i["actor_id"] not in l and "channel" not in i["actor_id"]:
+            l.append(i["actor_id"])
+        elif "from_id" in i and i["from_id"] not in l and "channel" not in i["from_id"]:
+            l.append(i["from_id"])
+    return l
+
+
+data = read("result.json") 
+print(find_all_users_id(data))
